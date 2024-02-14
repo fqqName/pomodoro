60394cd9fd8302e1394b30421de7ea122de345f7
fqqName <bogpezd@gmail.com>
2024-02-14 14:57:34 +0300

    create project v0.1 b1

---
+++ PomodoroV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PomodoroV2
+//
+//  Created by Pascal on 14.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+
+    }
+
+
+}
+
